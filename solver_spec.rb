7679fb13be3c6ce12148fa5dcf57a80711f951ee
Jonah Wambua <djmrjay@gmail.com>
2023-03-17 11:04:43 +0300

    Write tests for fizzbuzz method

--- solver_spec.rb
+++ solver_spec.rb
@@ -28,8 +28,23 @@ describe Solver do
       expect(Solver.new.reverse('')).to eq('')
     end
   end
+
+  describe '#fizzbuzz' do
+    it 'returns "fizz" when given 3' do
+      expect(Solver.new.fizzbuzz(3)).to eq('fizz')
+    end
+
+    it 'returns "buzz" when given 5' do
+      expect(Solver.new.fizzbuzz(5)).to eq('buzz')
+    end
+
+    it 'returns "fizzbuzz" when given 15' do
+      expect(Solver.new.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+
+    it 'returns "7" when given 7' do
+      expect(Solver.new.fizzbuzz(7)).to eq('7')
+    end
+  end
 end
 
-describe Solver do
-  
-end
